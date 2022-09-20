2982ab80ba9f9db0888e86c3113d4018c480c7e4
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-09-20 21:25:31 +0200

    Add FinalSpreadSheetGenerator

---
+++ MingelBingoCreator/Data/SpreadSheet.cs
@@ -0,0 +1,9 @@
+﻿namespace MingelBingoCreator.Data
+{
+    internal class SpreadSheet
+    {
+        public string Id { get; set; }
+        public List<string> Sheets { get; set; }
+        public object Name { get; internal set; }
+    }
+}
---
+++ MingelBingoCreator/FinalFileGenerator/FinalSpreadSheetGenerator.cs
@@ -0,0 +1,37 @@
+﻿using MingelBingoCreator.Configurations;
+using MingelBingoCreator.Data;
+using MingelBingoCreator.Repository;
+
+namespace MingelBingoCreator.FinalFileGenerator
+{
+    internal class FinalSpreadSheetGenerator
+    {
+        private readonly AppSettings _appSettings;
+        private readonly GoogleSheetsRepository _repository;
+
+        public FinalSpreadSheetGenerator(AppSettings appSettings, GoogleSheetsRepository repository)
+        {
+            _appSettings = appSettings;
+            _repository = repository;
+        }
+
+        internal SpreadSheet CreateFinalFile(List<MingelBingoCard> mingelBingoCards)
+        {
+            var result = _repository.CopyFile(_appSettings.GoogleSheetsOptions.TemplateSheetId, CreateNewFileName());
+
+            throw new NotImplementedException();
+            //Send board objects to copy of template sheet
+
+
+            //remove template tab from template sheet
+
+        }
+
+        private string CreateNewFileName()
+        {
+            var dateSuffix = DateTime.Now.ToString("MM-dd_HH-mm");
+
+            return $"{_appSettings.ExportOptions.FinalFileName}_{dateSuffix}";
+        }
+    }
+}
