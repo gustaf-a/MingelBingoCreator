9c9f3bcaa95a68c49928234576853aa00686880d
Gustaf Ahlner <gustaf.ahlner@gmail.com>
2022-09-22 20:03:02 +0200

    Add ValueRangeCreator

---
+++ MingeBingoCreatorUnitTests/RepositoryTests/GoogleSheetsRepositoryTests/ValueRangeCreatorTests.cs
@@ -0,0 +1,48 @@
+﻿using MingelBingoCreator.Data;
+using MingelBingoCreator.Repository.GoogleSheetsHelpers;
+using Xunit;
+
+namespace MingeBingoCreatorUnitTests.RepositoryTests.GoogleSheetsRepositoryTests
+{
+    public static class ValueRangeCreatorTests
+    {
+        [Fact]
+        public static void ValueRangeCreator_CreateValueRangesForCells_Returns_Correct_Values()
+        {
+            //Arrange
+            var sheetName = "testName";
+
+            var foundCells = new List<List<Cell>>();
+
+            for (int i = 1; i < 3; i++)
+            {
+                foundCells.Add(new List<Cell>());
+
+                for (int j = 1; j < 4; j++)
+                    foundCells.Last().Add(new Cell
+                    {
+                        RowIndex = i,
+                        ColumnIndex = j
+                    });
+            }
+
+            var values = new List<string>();
+
+            for (int i = 0; i < 6; i++)
+                values.Add(i.ToString());
+
+            //Act
+            var result = ValueRangeCreator.CreateValueRangesForCells(foundCells, values, sheetName);
+
+            //Assert
+            Assert.Single(result);
+
+            var returnedValues = result.First().Values;
+
+            Assert.Equal("0", returnedValues[0][0]);
+            Assert.Equal("2", returnedValues[0][2]);
+            Assert.Equal("3", returnedValues[1][0]);
+            Assert.Equal("5", returnedValues[1][2]);
+        }
+    }
+}
---
+++ MingelBingoCreator/Repository/GoogleSheetsHelpers/ValueRangeCreator.cs
@@ -0,0 +1,51 @@
+﻿using Google.Apis.Sheets.v4.Data;
+using MingelBingoCreator.Data;
+
+namespace MingelBingoCreator.Repository.GoogleSheetsHelpers
+{
+    internal static class ValueRangeCreator
+    {
+        internal static List<ValueRange> CreateValueRangesForCells(List<List<Cell>> foundCells, List<string> values, string sheetName)
+        {
+            var a1Notations = A1NotationCreator.GetA1NotationsForCells(foundCells, sheetName);
+
+            var valueRanges = new List<ValueRange>();
+
+            var valueTakenIndex = 0;
+
+            foreach (var a1Notation in a1Notations)
+            {
+                valueRanges.Add(new ValueRange
+                {
+                    Range = a1Notation.A1NotationString,
+                    Values = GetValuesOfSize(values, a1Notation.NumberOfRows, a1Notation.NumberOfColumns)
+                });
+
+                valueTakenIndex += a1Notation.NumberOfRows * a1Notation.NumberOfColumns;
+            }
+
+            return valueRanges;
+        }
+
+        private static IList<IList<object>> GetValuesOfSize(List<string> values, int rows, int columns)
+        {
+            var result = new List<IList<object>>();
+
+            var valuesTakenCounter = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                result.Add(new List<object>());
+
+                for (int j = 0; j < columns; j++)
+                {
+                    result[i].Add(values[valuesTakenCounter]);
+
+                    valuesTakenCounter++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
